b8aa3a4fc81208ed221b2493282d7dcc1bd42b55
Daniil <daniillishankov@gmail.com>
2026-05-20 09:36:26 +0200

    pages added

--- index.php
+++ index.php
@@ -5,16 +5,10 @@
 <div class="introduction">
     <h1>Welkom!!!</h1>
     <p>
-        <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet architecto at corporis et ipsa iste, labore libero modi mollitia nemo nihil nulla numquam porro praesentium quia quos sunt ullam voluptates!</span>
-        <span>Animi aperiam architecto cum dolor dolorem dolores ducimus eligendi est explicabo, fugit illum in laborum minus molestiae nam non quasi quidem recusandae rerum saepe sapiente similique ullam vel veniam voluptatibus.</span>
-        <span>Adipisci ipsam iure laboriosam maiores quis, rerum sapiente sunt. Asperiores blanditiis doloribus eos, laboriosam libero non numquam odit reprehenderit veniam? Alias fugit, in nesciunt nulla numquam perferendis perspiciatis quasi quos.</span>
-        <span>Architecto dolore ducimus eius laboriosam nemo nulla praesentium, quasi. Ad aspernatur deleniti inventore mollitia, perferendis possimus quis. Cupiditate dicta exercitationem iste laudantium voluptas? Accusamus animi beatae, distinctio harum iste quam?</span>
-        <span>Ducimus iste perferendis reprehenderit! Amet corporis delectus distinctio eos fugit harum inventore, iste porro quasi repellat reprehenderit temporibus ullam. Cupiditate explicabo nisi non quas quibusdam, ratione sed sint totam ullam!</span>
-        <span>Alias commodi dolorum ea incidunt nemo, obcaecati optio pariatur quaerat quo? Architecto consequuntur eos incidunt libero modi non optio quia quo soluta tenetur. Sequi, sit, totam. Accusantium dolore earum omnis.</span>
-        <span>Accusamus adipisci aspernatur consectetur cum, deleniti doloribus eligendi enim esse explicabo in ipsa ipsam ipsum labore magnam necessitatibus nesciunt non quae quod quos saepe soluta temporibus totam voluptate! Hic, ipsam!</span>
-        <span>A ab alias aliquam aliquid aperiam architecto consectetur consequuntur debitis distinctio dolor dolorem, exercitationem fugiat impedit ipsam labore magni minima nisi optio possimus repellendus sapiente tenetur ut voluptatem voluptates voluptatum?</span>
-        <span>Amet deserunt eius est ipsa iure perspiciatis quo rerum suscipit veritatis voluptatem! Ab adipisci blanditiis culpa illo ipsam ipsum iusto labore laboriosam, nam odit officiis, quidem, repudiandae sed temporibus totam.</span>
-        <span>Accusantium laborum odit totam. Animi eligendi nam quisquam rem reprehenderit? Dolore doloremque dolorum eveniet fugit hic, labore minima, mollitia nisi nulla numquam, praesentium ratione sapiente tempora totam voluptate. Assumenda, rem?</span>
+        <span>Lorem ipsum dolor sit amet, consectetur et oditia nemo nihil nulla numquam porro praesentium quia quos sunt ullam voluptates!</span>
+       <span>Accusantium laborum odit totam. Animi eligepre praesentium ratione sapiente tempora totam voluptate. Assumenda, rem?</span>
+        <span>Lorem ipsum dolor sit amet, consectetur et oditia nemo nihil nulla numquam porro praesentium quia quos sunt ullam voluptates!</span>
+       <span>Accusantium laborum odit totam. Animi eligepre praesentium ratione sapiente tempora totam voluptate. Assumenda, rem?</span>
     </p>
 </div>
 
