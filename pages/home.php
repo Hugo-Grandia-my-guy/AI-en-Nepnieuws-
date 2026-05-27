690253e3e0751ff6b144dbf7b18541f532cc8c5f
Hugo-Grandia-my-guy <hugodepdep6@gmail.com>
2026-05-27 09:50:32 +0200

    CSS and homepage
    this is the change for the texture and the homepage change

--- pages/home.php
+++ pages/home.php
@@ -5,12 +5,17 @@
 <h2>Wat is nepnieuws?</h2>
 <p>Nepnieuws is valse of misleidende informatie die wordt gepresenteerd als nieuws. Het kan worden verspreid via sociale media, websites, e-mails en andere digitale platforms. Nepnieuws kan verschillende vormen aannemen, zoals verzonnen verhalen, gemanipuleerde afbeeldingen of video's, en misleidende koppen.</p>
 <h2>Hoe kan AI worden gebruikt om nepnieuws te creëren en te verspreiden?</h2>
-<p>AI kan worden gebruikt om nepnieuws te creëren en te verspreiden op verschillende manieren
+<p>AI kan worden gebruikt om nepnieuws te creëren en te verspreiden op verschillende manieren</p>
 <ul>
+    <li><strong><a href="ai.php">AI</a></strong> wij gaan het hebben over AI. Op de site pagina's gaan we in detail in op de werking van AI en hoe het kan worden gebruikt in de context van nepnieuws.</li>
+    <li><strong><a href="photo-video.php">Photo & Video</a></strong> Wij gaan het hebben over de manier waarop AI kan worden gebruikt om foto's en video's te manipuleren en te verspreiden als nepnieuws.</li>
+    <li><strong><a href="tekst.php">Tekst</a></strong> Wij gaan het hebben over de manier waarop AI kan worden gebruikt om tekst te genereren en te verspreiden als nepnieuws.</li>
+</ul>
+<!--<ul>
     <li><strong>Automatisch gegenereerde inhoud:</strong> AI kan worden gebruikt om automatisch nepnieuwsartikelen, blogposts of social media-berichten te genereren. Deze inhoud kan er overtuigend uitzien en moeilijk te onderscheiden zijn van echte nieuwsberichten.</li>
     <li><strong>Deepfakes:</strong> AI-technologie kan worden gebruikt om realistische maar valse video's of afbeeldingen te maken, bekend als deepfakes. Deze kunnen worden gebruikt om mensen te misleiden of te manipuleren.</li>
     <li><strong>Sociale media bots:</strong> AI-aangedreven bots kunnen worden ingezet op sociale media om nepnieuws te verspreiden, door automatisch berichten te plaatsen, te delen of te liken, waardoor de zichtbaarheid van nepnieuws wordt vergroot.</li>
     <li><strong>Targeting en personalisatie:</strong> AI kan worden gebruikt om nepnieuws te targeten op specifieke groepen mensen op basis van hun interesses, gedrag of demografische gegevens, waardoor de kans groter wordt dat ze het geloven en delen.</li>
-</ul>
+</ul>-->
 <p>Het is belangrijk om kritisch te blijven en informatie te verifiëren voordat je het gelooft of deelt, vooral in het tijdperk van AI en nepnieuws. Controleer altijd de bron van het nieuws, zoek naar meerdere bronnen en wees voorzichtig met het delen van informatie die je niet kunt verifiëren.</p>
 
