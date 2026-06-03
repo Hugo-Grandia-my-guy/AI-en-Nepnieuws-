6d8b27e7d7be2dfc4afc90e11397f1952459b7da
Daniil <daniillishankov@gmail.com>
2026-06-03 11:25:36 +0200

    sitemap verbeteren
    🖼️🖼️🖼️

--- pages/ai.php
+++ pages/ai.php
@@ -1,19 +1,25 @@
 <h1>AI & Algoritmen</h1>
-<p>Kunstmatige intelligentie is tegenwoordig een krachtig instrument dat technologie, het bedrijfsleven en het dagelijks leven transformeert, maar het is ook een belangrijke bron van desinformatie.
-    Neurale netwerken zijn in staat om binnen enkele seconden hoogwaardige nepvideo's, <a href="index.php?page=photo_and_video">nepfoto's en -audio (deepfakes)</a> en <a href="index.php?page=tekst">nepteksten</a> te creëren, waardoor factchecking een complexe taak wordt.</p>
+<p>Kunstmatige intelligentie is tegenwoordig een krachtig instrument dat technologie, het bedrijfsleven en het dagelijks leven transformeert,
+    maar het is ook een belangrijke bron van desinformatie.
+    Neurale netwerken zijn in staat om binnen enkele seconden hoogwaardige nepvideo's, <a href="index.php?page=photo_and_video">nepfoto's en -audio (deepfakes)</a>
+    en <a href="index.php?page=tekst">nepteksten</a> te creëren, waardoor factchecking een complexe taak wordt.</p>
 <h3>De belangrijkste bedreigingen van AI op het gebied van informatietechnologie.</h3>
 <ul>
-    <li><strong><a href="index.php?page=photo_and_video">Deepfakes:</a></strong> Het genereren van nepvideo's en -audio met echte mensen die dingen zeggen of doen die ze nooit hebben gedaan. Dit wordt gebruikt om publieke figuren in diskrediet te brengen en fraude te plegen.</li>
-    <li><strong>Phishingautomatisering:</strong> Neurale netwerken schrijven overtuigende en gepersonaliseerde phishingberichten, waarmee standaard beveiligingssystemen worden omzeild.</li>
+    <li><strong><a href="index.php?page=photo_and_video">Deepfakes:</a></strong> Het genereren van nepvideo's en -audio met echte mensen
+        die dingen zeggen of doen die ze nooit hebben gedaan. Dit wordt gebruikt om publieke figuren in diskrediet te brengen en fraude te plegen.</li>
+    <li><strong>Phishingautomatisering:</strong> Neurale netwerken schrijven overtuigende en gepersonaliseerde phishingberichten,
+        waarmee standaard beveiligingssystemen worden omzeild.</li>
     <li><strong>Manipulatie van de publieke opinie:</strong> Massale creatie van nepnieuws en botnets om de werkelijkheid te verdraaien en de situatie te destabiliseren.</li>
-    <li><strong>Modelhallucinaties:</strong> De beste taalmodellen hebben de neiging om fictieve feiten voor echte aan te zien, waardoor het zelfs voor experts moeilijk is om de waarheid van gegenereerde leugens te onderscheiden.</li>
+    <li><strong>Modelhallucinaties:</strong> De beste taalmodellen hebben de neiging om fictieve feiten voor echte aan te zien,
+        waardoor het zelfs voor experts moeilijk is om de waarheid van gegenereerde leugens te onderscheiden.</li>
 </ul>
 <h3>Instrumenten om namaak te bestrijden</h3>
 <p>Er worden diverse tegenmaatregelen ontwikkeld om te beschermen tegen digitale misleiding:</p>
 <ul>
     <li><strong>Watermerken:</strong> Het inbedden van cryptografische tekens in afbeeldingen en tekst om de herkomst ervan te identificeren.</li>
     <li><strong>Feitencontrole:</strong> AI-algoritmen gebruiken om verdachte inhoud te monitoren en te identificeren.</li>
-    <li><strong>Digitale hygiëne:</strong> Kritisch denken blijft de belangrijkste verdediging. Het is essentieel om informatie te controleren aan de hand van officiële bronnen en twijfelachtig materiaal in twijfel te trekken.</li>
+    <li><strong>Digitale hygiëne:</strong> Kritisch denken blijft de belangrijkste verdediging. Het is essentieel om informatie te controleren aan de hand van officiële bronnen
+        en twijfelachtig materiaal in twijfel te trekken.</li>
 </ul>
 <h3>soorten bots</h3>
 <ul>
--- pages/home.php
+++ pages/home.php
@@ -1,22 +1,35 @@
 <h1><a href="index.php?page=ai">AI</a> en <a href="index.php?page=tekst">Nepnieuws</a></h1>
-<p>Welkom op onze website we gaan het hebben over <a href="index.php?page=ai">AI</a> en <a href="index.php?page=tekst">Nepnieuws</a>. We gaan het hebben over wat <a href="index.php?page=ai">AI</a> is, wat <a href="index.php?page=tekst">Nepnieuws</a> is en hoe <a href="index.php?page=ai">AI</a> kan worden gebruikt om <a href="index.php?page=tekst">Nepnieuws</a> te creëren en te verspreiden.</p>
+
+<p>Welkom op onze website we gaan het hebben over <a href="index.php?page=ai">AI</a> en <a href="index.php?page=tekst">Nepnieuws</a>.
+    We gaan het hebben over wat <a href="index.php?page=ai">AI</a> is,
+    wat <a href="index.php?page=tekst">Nepnieuws</a> is en hoe <a href="index.php?page=ai">AI</a> kan worden gebruikt om
+    <a href="index.php?page=tekst">Nepnieuws</a> te creëren en te verspreiden.</p>
+
 <h2>Wat is <a href="index.php?page=ai">AI</a>?</h2>
-<p><a href="index.php?page=ai">AI</a> staat voor Artificial Intelligence, wat in het Nederlands Kunstmatige Intelligentie betekent. Het is een tak van de informatica die zich bezighoudt met het creëren van machines en software die in staat zijn om taken uit te voeren die normaal gesproken menselijke intelligentie vereisen, zoals leren, redeneren, probleemoplossing en taalbegrip.</p>
+
+<p><a href="index.php?page=ai">AI</a> staat voor Artificial Intelligence, wat in het Nederlands Kunstmatige Intelligentie betekent.
+    Het is een tak van de informatica die zich bezighoudt met het creëren van machines en software die in staat zijn om taken uit te voeren
+    die normaal gesproken menselijke intelligentie vereisen, zoals leren, redeneren, probleemoplossing en taalbegrip.</p>
+
 <h2>Wat is <a href="index.php?page=tekst">Nepnieuws</a>?</h2>
-<p><a href="index.php?page=tekst">Nepnieuws</a> is valse of <a href="index.php?page=tekst">misleidende informatie</a> die wordt gepresenteerd als nieuws. Het kan worden verspreid via sociale media, websites, e-mails en andere digitale platforms. <a href="index.php?page=tekst">Nepnieuws</a> kan verschillende vormen aannemen, zoals verzonnen verhalen, <a href="index.php?page=photo_and_video">gemanipuleerde afbeeldingen of video's</a>, en <a href="index.php?page=tekst">misleidende koppen</a>.</p>
+
+<p><a href="index.php?page=tekst">Nepnieuws</a> is valse of <a href="index.php?page=tekst">misleidende informatie</a> die wordt gepresenteerd als nieuws.
+    Het kan worden verspreid via sociale media, websites, e-mails en andere digitale platforms. <a href="index.php?page=tekst">Nepnieuws</a> kan verschillende vormen aannemen,
+    zoals verzonnen verhalen, <a href="index.php?page=photo_and_video">gemanipuleerde afbeeldingen of video's</a>, en <a href="index.php?page=tekst">misleidende koppen</a>.</p>
+
 <h2>Hoe kan <a href="index.php?page=ai">AI</a> worden gebruikt om <a href="index.php?page=tekst">Nepnieuws</a> te creëren en te verspreiden?</h2>
+
 <p><a href="index.php?page=ai">AI</a> kan worden gebruikt om <a href="index.php?page=tekst">Nepnieuws</a> te creëren en te verspreiden op verschillende manieren</p>
+
 <ul>
-    <li><strong><a href="index.php?page=ai">AI</a></strong> wij gaan het hebben over <a href="index.php?page=ai">AI</a>. Op de site pagina's gaan we in detail in op de werking van <a href="index.php?page=ai">AI</a> en hoe het kan worden gebruikt in de context van nepnieuws.</li>
-    <li><strong><a href="index.php?page=photo_and_video">Photo & Video</a></strong> wij gaan het hebben over de manier waarop <a href="index.php?page=ai">AI</a> kan worden gebruikt om foto's en video's te manipuleren en te verspreiden als nepnieuws.</li>
-    <li><strong><a href="index.php?page=tekst">Tekst</a></strong> wij gaan het hebben over de manier waarop <a href="index.php?page=ai">AI</a> kan worden gebruikt om tekst te genereren en te verspreiden als nepnieuws.</li>
+    <li><strong><a href="index.php?page=ai">AI</a></strong> wij gaan het hebben over <a href="index.php?page=ai">AI</a>.
+        Op de site pagina's gaan we in detail in op de werking van <a href="index.php?page=ai">AI</a> en hoe het kan worden gebruikt in de context van nepnieuws.</li>
+    <li><strong><a href="index.php?page=photo_and_video">Photo & Video</a></strong> wij gaan het hebben over de manier
+        waarop <a href="index.php?page=ai">AI</a> kan worden gebruikt om foto's en video's te manipuleren en te verspreiden als nepnieuws.</li>
+    <li><strong><a href="index.php?page=tekst">Tekst</a></strong> wij gaan het hebben over de manier
+        waarop <a href="index.php?page=ai">AI</a> kan worden gebruikt om tekst te genereren en te verspreiden als nepnieuws.</li>
 </ul>
-<!--<ul>
-    <li><strong>Automatisch gegenereerde inhoud:</strong> AI kan worden gebruikt om automatisch nepnieuwsartikelen, blogposts of social media-berichten te genereren. Deze inhoud kan er overtuigend uitzien en moeilijk te onderscheiden zijn van echte nieuwsberichten.</li>
-    <li><strong>Deepfakes:</strong> AI-technologie kan worden gebruikt om realistische maar valse video's of afbeeldingen te maken, bekend als deepfakes. Deze kunnen worden gebruikt om mensen te misleiden of te manipuleren.</li>
-    <li><strong>Sociale media bots:</strong> AI-aangedreven bots kunnen worden ingezet op sociale media om nepnieuws te verspreiden, door automatisch berichten te plaatsen, te delen of te liken, waardoor de zichtbaarheid van nepnieuws wordt vergroot.</li>
-    <li><strong>Targeting en personalisatie:</strong> AI kan worden gebruikt om nepnieuws te targeten op specifieke groepen mensen op basis van hun interesses, gedrag of demografische gegevens, waardoor de kans groter wordt dat ze het geloven en delen.</li>
-</ul>-->
+
 <p>Het is belangrijk om kritisch te blijven en informatie te verifiëren voordat je het gelooft of deelt, vooral in het tijdperk van <a href="index.php?page=ai">AI</a> en <a href="index.php?page=tekst">Nepnieuws</a>. Controleer altijd de bron van het nieuws, zoek naar meerdere bronnen en wees voorzichtig met het delen van informatie die je niet kunt verifiëren.</p>
 
 <div class = 'test_container'>
