66a1c8fc128f4fa55325fb0f98638a42a1cb76dc
Hugo-Grandia-my-guy <hugodepdep6@gmail.com>
2026-06-11 14:59:39 +0200

    test AI

---
+++ pages/ai_test.php
@@ -0,0 +1,116 @@
+<h1>AI Quiz</h1>
+<h2>Welke Bot is het?</h2>
+<h3>Vraag #1</h3>
+<p>Ik maak systemen die automatisch inhoud te produceren, waaronder misleidende of valse informatie, wat de verificatie van bronnen bemoeilijkt.</p>
+<legend class="legend">Selecteer het juiste antwoord</legend>
+<div class="answers">
+    <div class="radio-option">
+        <input type="radio" name="Q1" id="Q1_A1" checked>
+        <label for="Q1_A1">generative AI</label>
+    </div>
+
+    <div class="radio-option">
+        <input type="radio" name="Q1" id="Q1_A2">
+        <label for="Q1_A2">chatbots</label>
+    </div>
+
+    <div class="radio-option">
+        <input type="radio" name="Q1" id="Q1_A3">
+        <label for="Q1_A3">social media bots</label>
+    </div>
+
+    <div class="radio-option">
+        <input type="radio" name="Q1" id="Q1_A4">
+        <label for="Q1_A4">video-generatoren</label>
+    </div>
+</div>
+<h2>Vraag #2</h2>
+<p>Het betreft systemen die automatisch video's kunnen creëren, inclusief deepfakes: gemanipuleerde video's die echt lijken maar niet authentiek zijn.</p>
+<legend class="legend">Selecteer het juiste antwoord</legend>
+<div class="answers">
+    <div class="radio-option">
+        <input type="radio" name="Q2" id="Q2_A1">
+        <label for="Q2_A1">generative AI</label>
+    </div>
+
+    <div class="radio-option">
+        <input type="radio" name="Q2" id="Q2_A2">
+        <label for="Q2_A2">chatbots</label>
+    </div>
+
+    <div class="radio-option">
+        <input type="radio" name="Q2" id="Q2_A3">
+        <label for="Q2_A3">social media bots</label>
+    </div>
+
+    <div class="radio-option">
+        <input type="radio" name="Q2" id="Q2_A4" checked> <!-- correct answer -->
+        <label for="Q2_A4">video-generatoren</label>
+    </div>
+</div>
+<h2>Vraag #3</h2>
+<p>Deze bots bootsen menselijke conversaties na en kunnen worden ingezet om desinformatie te verspreiden of gebruikers van onjuiste informatie te voorzien.</p>
+<legend class="legend">Selecteer het juiste antwoord</legend>
+<div class="answers">
+    <div class="radio-option">
+        <input type="radio" name="Q3" id="Q3_A1"> 
+        <label for="Q3_A1">generative AI</label>
+    </div>
+
+    <div class="radio-option">
+        <input type="radio" name="Q3" id="Q3_A2" checked> <!-- correct answer -->
+        <label for="Q3_A2">chatbots</label>
+    </div>
+
+    <div class="radio-option">
+        <input type="radio" name="Q3" id="Q3_A3">
+        <label for="Q3_A3">social media bots</label>
+    </div>
+
+    <div class="radio-option">
+        <input type="radio" name="Q3" id="Q3_A4">
+        <label for="Q3_A4">video-generatoren</label>
+    </div>
+</div>
+<h2>Vraag #4</h2>
+<h1>dit noemen ze modelhallucinaties:</h1>
+    <img src="assets/img/phishing_voorbeeld.webp" alt="Modelhallucinaties">
+<div>
+     <legend class="legend">Selecteer het juiste antwoord</legend>
+    <div class="answers">
+            <div class="radio-option">
+                <input type="radio" name="Q4" id="Q4_A1">
+                <label for="Q4_A1">True</label>
+            </div>
+
+            <div class="radio-option">
+                <input type="radio" name="Q4" id="Q4_A2" checked> <!-- correct answer -->
+                <label for="Q4_A2">False</label>
+            </div>
+    </div>
+           <!--  true/false statement, answer = false  -->
+</div>
+<h2>Vraag #5</h2>
+<p>Massale creatie van nepnieuws en botnets om de werkelijkheid te verdraaien en de situatie te destabiliseren.</p>
+<legend class="legend">Selecteer het juiste antwoord</legend>
+<div class="answers">
+    <div class="radio-option">
+        <input type="radio" name="Q5" id="Q5_A1" checked> 
+        <label for="Q5_A1">Watermerken</label>
+    </div>
+
+    <div class="radio-option">
+        <input type="radio" name="Q5" id="Q5_A2">
+        <label for="Q5_A2">modelhallucinaties</label>
+    </div>
+
+    <div class="radio-option">
+        <input type="radio" name="Q5" id="Q5_A3">
+        <label for="Q5_A3">manipulatie</label><!-- correct answer -->
+    </div>
+
+    <div class="radio-option">
+        <input type="radio" name="Q5" id="Q5_A4">
+        <label for="Q5_A4">Digitale hygiëne</label>
+    </div>
+</div>
