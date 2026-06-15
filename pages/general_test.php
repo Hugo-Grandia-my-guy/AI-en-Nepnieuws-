7e3a77a6caabc85dc2bb3f080a3a674e81793f46
Daniil <daniillishankov@gmail.com>
2026-06-15 13:21:43 +0200

    emergency push
    Hugo heeft gevragd

--- pages/general_test.php
+++ pages/general_test.php
@@ -1,13 +1,16 @@
 <h1>Verdieping test</h1>
 
     <h2>Vraag №1</h2>
-    <p>De afgelopen jaren is het gebruik van kunstmatige intelligentie (AI) in het onderwijs sterk toegenomen in scholen.
-    Door verschillende manieren is dat gebeurd namelijk door:
-    kennis te maken met Adobe Illustrastor je kan daar filters op maken helderheid toe te voegen en shaduwen te maken.</p>
+    <p>
+        De afgelopen jaren is het gebruik van kunstmatige intelligentie (AI) in het onderwijs sterk toegenomen in scholen.<br>
+    Door verschillende manieren is dat gebeurd namelijk door: <br>
+    kennis te maken met Adobe Illustrastor je kan daar filters op maken helderheid toe te voegen en shaduwen te maken.
+    </p>
+
         <legend class="legend">Selecteer het juiste antwoord</legend>
         <div class="answers">
             <div class="radio-option">
-                <input type="radio" name="Q1" id="Q1_A1" data-correct="true">
+                <input type="radio" name="Q1" id="Q1_A1">
                 <label for="Q1_A1">Watermerken</label>
             </div>
 
@@ -17,8 +20,8 @@
             </div>
 
             <div class="radio-option">
-                <input type="radio" name="Q1" id="Q1_A3" data-correct="true">
-                <label for="Q1_A3">Generative AI</label><!-- correct answer -->
+                <input type="radio" name="Q1" id="Q1_A3" data-correct="true"><!-- correct answer -->
+                <label for="Q1_A3">Generative AI</label>
             </div>
 
             <div class="radio-option">
@@ -32,7 +35,7 @@
          <legend class="legend">Selecteer het juiste antwoord</legend>
         <div class="answers">
             <div class="radio-option">
-                <input type="radio" name="Q2" id="Q2_A1" data-correct="true">
+                <input type="radio" name="Q2" id="Q2_A1">
                 <label for="Q2_A1">Generative AI</label>
             </div>
 
@@ -51,16 +54,31 @@
                 <label for="Q2_A4">Deeplearning</label>
             </div>
         </div>
+
     <h2>Vraag №3</h2>
 
     <h2>Photo & Video: Echt of AI?</h2>
 
     <p>Klik op de afbeelding die volgens jou **ECHT** is. Let goed op de details!</p>
 
-        <div class="quiz-question" id="q2">
-            <h3>Ronde 1: Welke berg is echt?</h3>
+<!--<h2>Vraag №4: Welk huis is echt?</h2>
 
-            <div class="quiz-options">
+    <legend class="legend">Selecteer echte foto</legend>
+    <div class="answers">
+        <div class="radio-option">
+            <input type="radio" name="Q4" id="Q4_A1" data-correct="true">
+            <img src="assets/img/house_real_quiz.jpg" alt="Huis 1">
+        </div>
+
+        <div class="radio-option">
+            <input type="radio" name="Q4" id="Q4_A2">
+            <img src="assets/img/house_AI_quiz.jpg" alt="Huis 2">
+        </div>
+    </div>-->
+        <div class="quiz-question" id="q3">
+            <h3>Welke berg is echt?</h3>
+
+            <div class="answers">
                 <div class="option-card" onclick="checkAnswer(1, 'echt', this)">
                     <img src="assets/img/berg_AI_quiz.jpg" alt="Berg 1">
                     <span class="label">Optie A</span>
@@ -71,8 +89,10 @@
                     <span class="label">Optie B</span>
                 </div>
             </div>
-            <div class="quiz-question" id="q2">
-                <h3>Ronde 1: Welk huis is echt?</h3>
+
+            <div class="quiz-question" id="q4">
+
+                <h3>Ronde 2: Welk huis is echt?</h3>
 
                 <div class="quiz-options">
                     <div class="option-card" onclick="checkAnswer(1, 'echt', this)">
@@ -85,83 +105,92 @@
                         <span class="label">Optie B</span>
                     </div>
                 </div>
-
-            <div class="quiz-feedback" id="feedback-1"></div>
+            </div>
         </div>
-     <h2>Vraag #4</h2> 
+
+     <h2>Vraag #4</h2>
+
      <p>Wat betekent “machine learning”?</p>
+
          <legend class="legend">Selecteer het juiste antwoord</legend>
         <div class="answers">
             <div class="radio-option">
-                <input type="radio" name="Q2" id="Q6_A1" checked>
+                <input type="radio" name="Q4" id="Q6_A1" >
                 <label for="Q2_A1">Machines leren zichzelf repareren</label>
             </div>
 
             <div class="radio-option">
-                <input type="radio" name="Q2" id="Q6_A2" checked> <!-- correct answer-->
+                <input type="radio" name="Q4" id="Q6_A2" data-correct="true"> <!-- correct answer-->
                 <label for="Q2_A2">computers leren patronen uit data kennen</label>
             </div>
 
             <div class="radio-option">
-                <input type="radio" name="Q2" id="Q6_A3">
+                <input type="radio" name="Q4" id="Q6_A3">
                 <label for="Q2_A3">Computers bouwen machines</label>
             </div>
 
             <div class="radio-option">
-                <input type="radio" name="Q2" id="Q6_A4">
+                <input type="radio" name="Q4" id="Q6_A4">
                 <label for="Q2_A4">Machines leren talen spreken zonder data</label>
             </div>
         </div>
+
         <h2>Vraag #5</h2>
+
         <p>Wat is data voor een AI-systeem?</p>
+
             <legend class="legend">Selecteer het juiste antwoord</legend>
                 <div class="answers">
                 <div class="radio-option">
-                    <input type="radio" name="Q2" id="Q3_A1" checked> <!-- correct answer -->
+                    <input type="radio" name="Q5" id="Q3_A1" data-correct="true"> <!-- correct answer -->
                     <label for="Q2_A1">Informatie waarmee AI kan leren en werken</label>
                 </div>
 
                 <div class="radio-option">
-                    <input type="radio" name="Q2" id="Q3_A2">
+                    <input type="radio" name="Q5" id="Q3_A2">
                     <label for="Q2_A2">Een soort computerchip</label>
                 </div>
 
                 <div class="radio-option">
-                    <input type="radio" name="Q2" id="Q3_A3">
+                    <input type="radio" name="Q5" id="Q3_A3">
                     <label for="Q2_A3">een programmeertaal</label>
                 </div>
 
                 <div class="radio-option">
-                    <input type="radio" name="Q2" id="Q4_A4">
+                    <input type="radio" name="Q5" id="Q4_A4">
                     <label for="Q2_A4">een internetkabel</label>
                 </div>
             </div>
+
         <h2>Vraag #6</h2>
+
         <p>Wat is een belangrijk ethisch risico van generatieve AI?</p>
+
           <legend class="legend">Selecteer het juiste antwoord</legend>
           <div class="answers">
               <div class="radio-option">
-                  <input type="radio" name="Q2" id="Q5_A1" checked><!-- correct answer -->
+                  <input type="radio" name="Q6" id="Q5_A1" data-correct="true"><!-- correct answer -->
                   <label for="Q2_A1">Bias en discriminatie door trainingsdata</label>
               </div>
 
               <div class="radio-option">
-                  <input type="radio" name="Q2" id="Q5_A2">
+                  <input type="radio" name="Q6" id="Q5_A2">
                   <label for="Q2_A2">Te veel schermhelderheid</label>
               </div>
 
               <div class="radio-option">
-                  <input type="radio" name="Q2" id="Q5_A3">
+                  <input type="radio" name="Q6" id="Q5_A3">
                   <label for="Q2_A3">Langzame Wifi</label>
               </div>
 
               <div class="radio-option">
-                  <input type="radio" name="Q2" id="Q5_A4">
+                  <input type="radio" name="Q6" id="Q5_A4">
                   <label for="Q2_A4">Te weinig opslagruimte</label>
               </div>
           </div>
+
 <div class="bot_button">
-    <a href="index.php?page=score" onclick="finishTest('text', 4)">
+    <a href="index.php?page=score" onclick="finishTest('text', 6)">
         Test afronden
     </a>
 </div>
